1f464e104ec3991d6402dec8cb80eca526abd5a6
Baris Vural <bariscan.vural@bloomreach.com>
2018-07-06 14:00:44 +0200

    Revert "Refactor persistant volume yaml files; Simplify scripts"
    This reverts commit 5306073305f2452e378ba46e14da1e3a4e1eb62f.

--- kubernetes/delete-all.sh
+++ kubernetes/delete-all.sh
@@ -4,9 +4,4 @@ kubectl delete deployment mysql
 kubectl delete service mysql
 kubectl delete service,deployment backatcha-server
 kubectl delete service,deployment hippo
-kubectl delete ingress ingress-site
-kubectl delete ingress ingress-cms
-kubectl delete ingress ingress-backatcha
-kubectl delete pvc mysql-pv-claim
-#kubectl delete pv task-pv-volume
-
+kubectl delete ingress ingress-tutorial
--- kubernetes/deploy-all.sh
+++ kubernetes/deploy-all.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 
+#kubectl delete pv mysql-pv-volume
+#kubectl delete pvc mysql-pv-claim
+kubectl delete deployment mysql
+kubectl delete service mysql
+kubectl delete service,deployment backatcha-server
+kubectl delete service,deployment hippo
+kubectl delete ingress ingress-site
+kubectl delete ingress ingress-cms
+kubectl delete ingress ingress-backatcha
+
+#kubectl create -f mysql-pv.yaml #Create once and don't delete every time
 kubectl create -f ingress.yaml
 kubectl create -f mysql-deployment.yaml
 kubectl create -f backatcha-deployment.yaml
 kubectl create -f hippo-deployment.yaml
+
+
