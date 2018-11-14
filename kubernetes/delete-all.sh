d000a435abcd764e359620e4144a8e3fca714362
Baris Vural <bariscan.vural@bloomreach.com>
2018-11-14 13:36:42 +0100

    Minor changes

--- kubernetes/delete-all.sh
+++ kubernetes/delete-all.sh
@@ -4,6 +4,7 @@ kubectl delete deployment mysql
 kubectl delete service mysql
 kubectl delete service,deployment backatcha-server
 kubectl delete service,deployment hippo
+kubectl delete service,deployment site-only
 kubectl delete ingress ingress-site
 kubectl delete ingress ingress-cms
 kubectl delete ingress ingress-backatcha
